ecd6f8fa055c2d3b3a2c663bd625fffce3a414cd
Mashudu <62095778+MashuduWW@users.noreply.github.com>
2025-07-10 19:40:15 +0200

    Added two asynchronous functions "asyncfetchusers()" and "asyncfetcholder_users()".

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -1,32 +1,32 @@
-
-# first "pip install aiosqlite" in the virtual environment
-
 import aiosqlite
 import asyncio
 
-DB_NAME = "example.db"
+DB_NAME = "airbnb.db"
 
-async def async_fetch_users():
+async def asyncfetchusers():
     async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute("SELECT * FROM users") as cursor:
             users = await cursor.fetchall()
-            print("\nAll Users:")
-            for user in users:
-                print(user)
+            return users  #  Return all users
 
-async def async_fetch_older_users():
+async def asyncfetcholder_users():
     async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
             older_users = await cursor.fetchall()
-            print("\nUsers Older Than 40:")
-            for user in older_users:
-                print(user)
+            return older_users  #  Return users older than 40
 
 async def fetch_concurrently():
-    await asyncio.gather(
-        async_fetch_users(),
-        async_fetch_older_users()
+    users, older_users = await asyncio.gather(
+        asyncfetchusers(),
+        asyncfetcholder_users()
     )
+    print("All Users:")
+    for user in users:
+        print(user)
+
+    print("\nUsers Older Than 40:")
+    for user in older_users:
+        print(user)
 
 # --- Run the concurrent fetch ---
 asyncio.run(fetch_concurrently())
