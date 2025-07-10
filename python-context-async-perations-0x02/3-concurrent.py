df8512454b00ac46d7fbe4884533bf7bf0fa21cd
Mashudu <62095778+MashuduWW@users.noreply.github.com>
2025-07-10 19:33:48 +0200

    Created Concurrent Asynchronous Database Query.

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,32 @@
+
+# first "pip install aiosqlite" in the virtual environment
+
+import aiosqlite
+import asyncio
+
+DB_NAME = "example.db"
+
+async def async_fetch_users():
+    async with aiosqlite.connect(DB_NAME) as db:
+        async with db.execute("SELECT * FROM users") as cursor:
+            users = await cursor.fetchall()
+            print("\nAll Users:")
+            for user in users:
+                print(user)
+
+async def async_fetch_older_users():
+    async with aiosqlite.connect(DB_NAME) as db:
+        async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
+            older_users = await cursor.fetchall()
+            print("\nUsers Older Than 40:")
+            for user in older_users:
+                print(user)
+
+async def fetch_concurrently():
+    await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+
+# --- Run the concurrent fetch ---
+asyncio.run(fetch_concurrently())
