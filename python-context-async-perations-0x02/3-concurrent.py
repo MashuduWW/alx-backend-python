4bd6d4604f9a0245481985b45a260b29ba04f626
Mashudu <62095778+MashuduWW@users.noreply.github.com>
2025-07-10 19:45:26 +0200

    Renamed asynchronous functions.

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -7,19 +7,22 @@ async def asyncfetchusers():
     async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute("SELECT * FROM users") as cursor:
             users = await cursor.fetchall()
-            return users  #  Return all users
+            return users
 
 async def asyncfetcholder_users():
     async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
             older_users = await cursor.fetchall()
-            return older_users  #  Return users older than 40
+            return older_users
 
 async def fetch_concurrently():
-    users, older_users = await asyncio.gather(
+    results = await asyncio.gather(
         asyncfetchusers(),
         asyncfetcholder_users()
     )
+
+    users, older_users = results
+
     print("All Users:")
     for user in users:
         print(user)
@@ -28,5 +31,5 @@ async def fetch_concurrently():
     for user in older_users:
         print(user)
 
-# --- Run the concurrent fetch ---
+# Run the concurrent fetch
 asyncio.run(fetch_concurrently())
