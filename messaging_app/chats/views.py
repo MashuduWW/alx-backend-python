305249fabab5dcb35bb83b120b57b536982e5665
Mashudu <62095778+MashuduWW@users.noreply.github.com>
2025-07-18 19:33:30 +0200

    Updated views.py to allow Search, Sort, Filter and return clear HTTP codes

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,26 +1,31 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, status, filters
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
-from rest_framework.response import Response
-from rest_framework.decorators import action
 
 class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.prefetch_related('participants', 'messages').all()
+    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['participants__username']
+    ordering_fields = ['created_at']
+    ordering = ['-created_at']
 
     def perform_create(self, serializer):
-        # Automatically add the request user as a participant if not manually passed
         conversation = serializer.save()
-        conversation.participants.add(self.request.user)
+        return Response(ConversationSerializer(conversation).data, status=status.HTTP_201_CREATED)
+
 
 class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        conversation_id = self.kwargs.get('conversation_pk')
-        return Message.objects.filter(conversation_id=conversation_id)
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['sent_at']
+    ordering = ['-sent_at']
 
     def perform_create(self, serializer):
-        serializer.save(sender=self.request.user, conversation_id=self.kwargs['conversation_pk'])
+        serializer.save(sender=self.request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
